e4fd243c8c5bc68b16d63533f9975253d2907177
Deepa Thiagarajan <deepa.name@gmail.com>
2020-03-01 00:03:15 -0800

    sending email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,9 +1,17 @@
 import { ScrollView, Text } from 'react-native';
 import React, { Component } from 'react';
-import { Card } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import { Card, Button, Icon } from 'react-native-elements';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ['campsites@nucamp.co'],
+      subject: 'Inquiry',
+      body: 'To whom it may concern:'
+    });
+  }
   state = {};
   render() {
     return (
@@ -14,6 +22,19 @@ class Contact extends Component {
               1 Nucamp Way{'\n'}Seattle, WA 98001{'\n'}U.S.A. {'\n\n'}Phone:
               1-206-555-1234{'\n'}Email: campsites@nucamp.co
             </Text>
+            <Button
+              title='Send Email'
+              buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+              icon={
+                <Icon
+                  name='envelope-o'
+                  type='font-awesome'
+                  color='#fff'
+                  iconStyle={{ marginRight: 10 }}
+                />
+              }
+              onPress={() => this.sendMail()}
+            />
           </Card>
         </Animatable.View>
       </ScrollView>
